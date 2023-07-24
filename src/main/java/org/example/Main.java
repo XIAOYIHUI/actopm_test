317c59dadb745d3514fb64b1b3914d7b359b2c73
XIAO <2066238362@qq.com>
2023-07-24 16:49:30 +0800

    first try

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,11 @@
+package org.example;
+
+import java.util.logging.Logger;
+
+public class Main {
+    private static final Logger logger = Logger.getLogger(String.valueOf(Main.class));
+
+    public static void main(String[] args) {
+        logger.info("Hello world!");
+    }
+}
